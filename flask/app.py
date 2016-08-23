ca063a54e47e0154d38d4794723d4945574d730c
asrivastava <asrivastava@aconex.com>
2016-08-23 09:53:03 +0530

    Added functionality to add data in postgres db

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2:///demo'
+db = SQLAlchemy(app)
 
-@app.route("/")
-def hello():
+# Create our database model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, email):
+        self.email = email
+
+    def __repr__(self):
+        return '<E-mail %r>' % self.email
+
+@app.route("/", methods=['GET', 'POST'])
+def main():
+    if request.method == 'POST':
+        email = request.form['email']
+        # Check that email does not already exist (not a great query, but works)
+        if not db.session.query(User).filter(User.email == email).count():
+            reg = User(email)
+            db.session.add(reg)
+            db.session.commit()
+    
     return render_template('index.html')
 
 
